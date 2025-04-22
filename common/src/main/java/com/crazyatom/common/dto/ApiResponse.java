4b35ebdacea5bb1f51ae6a8c00bf734e57645cc9
강현부(SK컴즈) <kanghb@sk.com>
2025-04-22 13:30:37 +0900

    springdoc root에서 공통 적용

--- common/src/main/java/com/crazyatom/common/dto/ApiResponse.java
+++ common/src/main/java/com/crazyatom/common/dto/ApiResponse.java
@@ -1,16 +1,21 @@
 package com.crazyatom.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(description = "API 응답 객체")
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
 
-	private final boolean success;        // 성공 여부
-	private final T data;                // 성공 시 데이터
-	private final ErrorResponse error;    // 실패 시 에러 정보
+	@Schema(description = "성공 여부")
+	private final boolean success;
+	@Schema(description = "성공 시 응답 데이터")
+	private final T data;
+	@Schema(description = "실패 시 에러 정보")
+	private final ErrorResponse error;
 
 	public static <T> ApiResponse<T> success(T data) {
 		return new ApiResponse<>(true, data, null);
